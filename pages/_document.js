3b5f0ee1a13b4b879ae33f47c4dd4c8e85214441
omarfarook <md.omarfarook.v.m@gmail.com>
2020-01-28 15:38:40 +0530

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+          <link rel="shortcut icon" type="image/ico" href="../static/favicon.ico"/>
           <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
           <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
         </Head>
