4854b8dda14677ef989b1af56785eb5b243ea227
omarfarook <md.omarfarook.v.m@gmail.com>
2020-01-31 22:42:33 +1100

    styles updated

--- constant/style.js
+++ constant/style.js
@@ -1,2 +1,4 @@
-export const LIGHT_RED = 'rgba(145,13,13,.8)'
+export const LIGHT_REDa = 'rgba(145,13,13,.8)'
+export const LIGHT_RED = 'rgba(5, 51, 109, 0.8)'
 export const FORMAL_WHITE = 'white'
+export const BG_GRADIENT = 'linear-gradient(to top, #dfe9f3 0%, white 100%)'
