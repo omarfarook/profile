7a5a727bb6b3624e5e9bb0916bdcdeef95562b8d
Mohamed Omar Farook <omar.farook@aspiredigital.com>
2021-08-20 16:26:58 +0800

    profile updated

--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ const nextRoutes = require('next-routes')
 const routes = module.exports = nextRoutes()
 
 routes.add('/contactme', '/contactme')
+routes.add('/aboutme', '/aboutme')
 routes.add('mycv', '/mycv')
