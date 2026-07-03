bc40d5fe7743bd6377ea1ef0011b0ae119ceba39
omarfarook <md.omarfarook.v.m@gmail.com>
2026-07-03 12:06:28 +0800

    Add Sentry monitoring setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 const nextConfig = {
   turbopack: {
     root: __dirname
   }
 }
 
-module.exports = nextConfig
+module.exports = withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: !process.env.CI,
+  sourcemaps: {
+    disable: !process.env.SENTRY_AUTH_TOKEN
+  }
+})
