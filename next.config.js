4cfe8f8fc0dad068aeeb42bc4df5b50841414667
omarfarook <md.omarfarook.v.m@gmail.com>
2026-07-03 12:18:37 +0800

    Add direnv Sentry env template

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
 }
 
 module.exports = withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
+  org: process.env.NEXT_PUBLIC_SENTRY_ORG,
+  project: process.env.NEXT_PUBLIC_SENTRY_PROJECT,
   authToken: process.env.SENTRY_AUTH_TOKEN,
   silent: !process.env.CI,
   sourcemaps: {
